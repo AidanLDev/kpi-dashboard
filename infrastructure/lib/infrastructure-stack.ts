2d123b7c886ae837eada8799eef9b05f99553e20
Aidan Lowson <aidan.lowson@processvision.com>
2026-05-22 15:32:45 +0100

    Creating timestream db and table

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,16 +1,34 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as timestream from "aws-cdk-lib/aws-timestream";
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const timestreamDb = new timestream.CfnDatabase(
+      this,
+      "KpiDashboardDatabase",
+      {
+        databaseName: "KpiDashboardDatabase",
+      },
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Retain database if stack is deleted
+    timestreamDb.applyRemovalPolicy(cdk.RemovalPolicy.RETAIN);
+
+    const timestreamTable = new timestream.CfnTable(this, "KpiMetricsTable", {
+      databaseName: "KpiDashboardDatabase",
+      tableName: "KpiMetrics",
+      retentionProperties: {
+        memoryStoreRetentionPeriodInHours: (24 * 31).toString(),
+        magneticStoreRetentionPeriodInDays: (365 * 5).toString(),
+      },
+    });
+
+    // Retain table if stack is deleted
+    timestreamTable.applyRemovalPolicy(cdk.RemovalPolicy.RETAIN);
+
+    timestreamTable.node.addDependency(timestreamDb);
   }
 }
